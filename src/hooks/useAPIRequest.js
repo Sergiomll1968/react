f2a1e17b658fa2a055bdb9934c9b933144f0297b
sergiomll1968 <sergiomaciaslluch@gmail.com>
2023-12-12 09:33:58 +0100

    act 20231212 0933

--- src/hooks/useAPIRequest.js
+++ src/hooks/useAPIRequest.js
@@ -4,7 +4,10 @@ export function useAPIRequest() {
   const [data, setData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-
+  const arrowUp = String.fromCodePoint(0x21E7);
+  const arrowDown = String.fromCodePoint(0x21E9);
+  const deletefilterIcon = String.fromCodePoint(0x26D2);
+  const [order, setOrder] = useState([0,arrowUp]) // 0 -> Not ordered, 1 -> Ascending, 2 -> Descending
 
   async function getData({ route, method = 'GET', searchType, searchString, body, headers = {} }) {
     setData();
@@ -38,8 +41,14 @@ export function useAPIRequest() {
               filteredData = responseAsJson;
               break;
           }
-          setData(filteredData);
-          // setData(responseAsJson);
+          const users = [
+            { Name: 'Kevin', Age: 42, Profits: 500, Expenses: 200, Gap: 300 },
+            { Name: 'Peter', Age: 30, Profits: 250, Expenses: 50, Gap: 200 },
+            { Name: 'Luise', Age: 50, Profits: 150, Expenses: 30, Gap: 120 },
+            { Name: 'Jules', Age: 18, Profits: 425, Expenses: 180, Gap: 245 },
+            { Name: 'Carol', Age: 37, Profits: 800, Expenses: 425, Gap: 375 }
+          ];
+          setData(users);
         } else {
           setError(new Error(`${response.status}: ${response.statusText}`));
           // error === { message: '401: Unathorized' }
@@ -49,8 +58,8 @@ export function useAPIRequest() {
       } finally {
         setLoading(false);
       }
-      
-      return { getData, data, error, loading };
+      setOrder(order[0] === 0? [1,arrowDown] : order[0] === 1 ? [2,deletefilterIcon] : [0,arrowUp]);
+      return { getData, data, error, loading, order };
     }
     // *******************************************************************************************
     if (searchType === 'filter') {
--- src/pages/UseAPIRequest/UseAPIRequest.jsx
+++ src/pages/UseAPIRequest/UseAPIRequest.jsx
@@ -1,19 +1,20 @@
 import './UseAPIRequest.css'
 import { useAPIRequest } from '../../hooks/useAPIRequest.js';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 // import "gridjs/dist/theme/mermaid.css";
 
 function UseAPIRequest() {
-  const { getData, data, error, loading } = useAPIRequest();
-  const arrowUp = String.fromCodePoint(0x21E7);
-  const arrowDown = String.fromCodePoint(0x21E9);
-  const deletefilterIcon = String.fromCodePoint(0x26D2);
-  const [order, setOrder] = useState([0,arrowUp]) // 0 -> No ordenado, 1 -> Ascendente, 2 -> Descendente
+  const { getData, data, error, loading, order=[0,String.fromCodePoint(0x21E7)]} = useAPIRequest();
+  // const arrowUp = String.fromCodePoint(0x21E7);
+  // const arrowDown = String.fromCodePoint(0x21E9);
+  // const deletefilterIcon = String.fromCodePoint(0x26D2);
+  // const [order, setOrder] = useState([0,arrowUp]) // 0 -> Not ordered, 1 -> Ascending, 2 -> Descending
 
   function sortTable() {
-    getData({searchString: order[0], searchType: 'order'});
-    setOrder(order[0] === 0? [1,arrowDown] : order[0] === 1 ? [2,deletefilterIcon] : [0,arrowUp]);
+    getData({searchString: (order[0]), searchType: 'order'});
+    console.log(order);
+    // setOrder(order[0] === 0? [1,arrowDown] : order[0] === 1 ? [2,deletefilterIcon] : [0,arrowUp]);
   }
   
   return (
@@ -56,14 +57,15 @@ function UseAPIRequest() {
           <tbody>
             {data && data.map((element,idx) =>
             <tr key={idx}>
-              <td>{Object.values(element)[0]}</td>
+              {data && Object.values(element).map((subelement,idx) => <td key={idx} >{subelement}</td>)}
+              {/* <td>{Object.values(element)[0]}</td>
               <td>{Object.values(element)[1]}</td>
               <td>{Object.values(element)[2]}</td>
               <td>{Object.values(element)[3]}</td>
               <td>{Object.values(element)[4]}</td>
               <td>{Object.values(element)[5] === false ? 'No' : 'Yes'}</td>
               <td>{Object.values(element)[6]}</td>
-              <td>{Object.values(element)[7]}</td>
+              <td>{Object.values(element)[7]}</td> */}
             </tr>
             )}
           </tbody>
